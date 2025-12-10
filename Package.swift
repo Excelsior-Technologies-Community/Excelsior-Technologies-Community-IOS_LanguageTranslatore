9d3039eb8857f1c8c07e53f32e0dbc29c64a987f
noman1303 <belimnoman1303@gmail.com>
2025-12-10 13:46:16 +0530

    PackegFile

--- Package.swift
+++ Package.swift
@@ -1,31 +1,26 @@
-// LiveTranslationKit/Package.swift
-
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Package.swift
+// MUST be the first line!
+// ----------------------
+// swift-tools-version: 5.9 
+// ----------------------
 
 import PackageDescription
 
 let package = Package(
-    name: "LiveTranslationKit",
-    
-    // Define supported platforms (iOS 15 or later for SwiftUI .task, .async/await)
+    name: "LiveTranslationKit", // Use this name, or whatever you put in Package.swift
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12)
+        .iOS(.v15), 
+        .macOS(.v12) // Or whatever platforms you need
     ],
-    
     products: [
-        // This is what users will import (e.g., `import LiveTranslationKit`)
         .library(
             name: "LiveTranslationKit",
             targets: ["LiveTranslationKit"]),
     ],
-    
     targets: [
         .target(
             name: "LiveTranslationKit",
             dependencies: [],
-            // Set the path to your source files
             path: "Sources/LiveTranslationKit"
         ),
     ]
